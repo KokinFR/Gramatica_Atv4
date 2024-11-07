fa766478ea76010d9be1417effbd7fcca7e53192
KokinFR <franciscorafaelzero78@gmail.com>
2024-11-07 11:22:50 -0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+1 – Implemente um método em Java que verifique se uma cadeia de caracteres pertence
+à linguagem definida ela gramática regular abaixo:
+Detalhes da Gramática:
+• A gramática é definida pelas seguintes produções:
+o S → aA | bB
+o A → aA | ε
+o B → bB | ε
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
